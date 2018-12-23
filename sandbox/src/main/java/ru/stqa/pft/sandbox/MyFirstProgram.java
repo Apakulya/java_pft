b6caeb8e8505efce74cb014f825b20492d084129
Apakulya <46006851+Apakulya@users.noreply.github.com>
2018-12-23 20:18:11 +0300

    дз1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    //hello("Настя");
+    Square s = new Square(6);
+    System.out.println("Площадь квадрата со стороной " + s.l + " =: " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " =: " + r.area());
   }
 
+  public static void hello(String something) {
+    System.out.println("Hello" + " " + something + " Have a nice day!");
+  }
+
+
+  public static double area(Rectangle r)
+  {
+    return r.a*r.b;
+  }
 }
+
+
+
