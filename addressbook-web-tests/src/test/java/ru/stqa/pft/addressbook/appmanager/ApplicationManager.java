6bdf21bd637cd2752fcf413f2612dc95ef714a44
Apakulya <46006851+Apakulya@users.noreply.github.com>
2018-12-28 15:06:45 +0300

    Добавила запуск в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,13 +17,24 @@ public class ApplicationManager {
   private ContactHelper сontactHelper;
   private SessionHelper sessionHelper;
   private String baseUrl;
+  private String browser;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
+
+  public ApplicationManager(String browser) {
+    this.browser=browser;
+  }
 
   public void init() {
     System.setProperty("webdriver.gecko.driver","C:\\Users\\Asus\\Documents\\Geckodriver\\geckodriver.exe");
     System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-    driver = new FirefoxDriver();
+    if (browser==BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    } else if (browser==BrowserType.CHROME) {
+      driver = new ChromeDriver();
+    } else if (browser==BrowserType.IE){
+      driver = new InternetExplorerDriver();
+    }
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/group.php");
