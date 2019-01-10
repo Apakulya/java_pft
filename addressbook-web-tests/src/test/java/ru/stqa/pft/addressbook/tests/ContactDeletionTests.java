6ec2866a71ff354c502375946ec3ff1fd11c8176
Apakulya <46006851+Apakulya@users.noreply.github.com>
2019-01-10 13:29:44 +0300

    Реализованы проверки путём сравнения списков в тестах для удаления и модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
   @Test
@@ -11,9 +14,14 @@ public class ContactDeletionTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("Илья","Ильич","Luxoft",
               "25", "February", "1992", "666"));
     }
-
-    app.getContactHelper().selectContact();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().returntoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size()-1);
+
+    before.remove(before.size()-1);
+    Assert.assertEquals(before,after);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEditionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEditionTests.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactEditionTests extends TestBase{
   @Test
@@ -11,11 +16,22 @@ public class ContactEditionTests extends TestBase{
       app.getContactHelper().createContact(new ContactData("Илья","Ильич","Luxoft",
               "25", "February", "1992", "666"));
     }
-    app.getContactHelper().selectContact();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().editContact();
-    app.getContactHelper().fillGontactForm(new ContactData("Тест","Тестович","Luxoft",
-            "25", "February", "1992", null), false);
+    ContactData contact = new ContactData("Тест","Тестович","Luxoft",
+            "25", "February", "1992", null);
+    app.getContactHelper().fillGontactForm(contact, false);
     app.getContactHelper().submitContactEdit();
     app.getContactHelper().returntoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
   }
 }
